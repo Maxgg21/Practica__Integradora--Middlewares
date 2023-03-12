08d820bd93ad079eb736081d4f44d2b1dfd7a458
Maxgg21 <lionel.gegena@gmail.com>
2023-03-11 21:42:04 -0300

    Subiendo middleware de registracion de rutas y otro middleware de acceso a los administradores

---
+++ src/controllers/mainController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    index: (req, res) => res.render('index', {
+        title: 'INDEX'
+    }),
+    admin: (req, res) => res.render('admin', {
+        name: req.query.admin,
+        title: 'ADMIN'
+    }),
+}
